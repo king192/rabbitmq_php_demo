234821a23526a281ddd664252337f70bc221de17
xhq <1293812979@qq.com>
2019-04-18 18:27:30 +0800

    Create py_consumer.py
    Any language

---
+++ py_consumer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import pika
+import time
+
+credentials = pika.PlainCredentials('guest', 'guest')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello', durable=False)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='hello', on_message_callback=callback)
+
+channel.start_consuming()
